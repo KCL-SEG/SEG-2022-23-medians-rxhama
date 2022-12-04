f6a01520512cfb6fbce1a82949c373c56220c270
Roland Xhama <rxhama02@gmail.com>
2022-12-04 16:21:35 +0000

    solution1

--- median.py
+++ median.py
@@ -5,6 +5,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
         if len(numbers)%2 == 1:
             medianVal = numbers[int((len(numbers) - 1) / 2)]
         else:
