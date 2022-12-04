d745d06b590ad23dfb23ef9900beaaa30f842600
Roland Xhama <rxhama02@gmail.com>
2022-12-04 16:05:33 +0000

    3

--- median.py
+++ median.py
@@ -5,16 +5,14 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        if len(numbers)%2 == 1:
+            medianVal = numbers[(len(numbers) - 1) / 2]
+        else:
+            temp1 = numbers[(len(numbers) - 2) / 2]
+            temp2 = numbers[len(numbers) / 2]
+            medianVal = (temp1 + temp2) / 2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
-
-if len(numbers)%2 == 1:
-    medianVal = numbers[(len(numbers) - 1) / 2]
-else:
-    temp1 = numbers[(len(numbers) - 2) / 2]
-    temp2 = numbers[len(numbers) / 2]
-    medianVal = (temp1 + temp2) / 2
 print(medianVal)
